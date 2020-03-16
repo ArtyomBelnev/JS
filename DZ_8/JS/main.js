c5d2ea89f0a3ce3e8c64c6e784dd2a406f9c1e06
Belnev <belnevtema@gmail.com>
2020-03-16 21:17:10 +0300

    первое задание

---
+++ DZ_8/JS/main.js
@@ -0,0 +1,49 @@
+function Animal(name) {
+    this._foodAmount = 50;
+    this._name = name;
+    this._self = this;
+}
+
+Animal.prototype._formatFoodAmount = function() {
+    return this._foodAmount + ' гр.';
+};
+
+Animal.prototype.dailyNorm = function(amount) {
+    if(!arguments.length) return this._formatFoodAmount();
+
+    if (amount < 50 || amount > 500) {
+        return 'Недопустимое количество корма.';
+    }
+
+    this._foodAmount = amount;
+};
+
+Animal.prototype.feed = function() {
+    console.log('Насыпаем в миску ' + this._self.dailyNorm() + ' корма.');
+};
+
+function Cat(name) {
+    Animal.apply(this, arguments);
+}
+
+Cat.prototype = Object.create(Animal.prototype);
+Cat.prototype.constructor = Cat;
+
+Cat.prototype.feed = function() {
+    Animal.prototype.feed.apply(this, arguments);
+    console.log('Кот доволен ^_^');
+    return this;
+};
+
+Animal.prototype.stroke = function() {
+    console.log('Гладим кота');
+    return this;
+}
+
+
+var cat = new Cat('Tom');
+
+console.log(cat._formatFoodAmount());
+cat.dailyNorm(143);
+cat.feed().stroke();
+
