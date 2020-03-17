05840cc8f3a63b7f8bd79a0da348d70f0069c2d8
Belnev <58457425+Belnev@users.noreply.github.com>
2020-03-17 14:56:13 +0300

    Update main.js

--- Task/js/main.js
+++ Task/js/main.js
@@ -36,3 +36,37 @@ console.log(barsik.dailyNorm(250));
 console.log(barsik.feed());
 
 barsik = null;
+
+// function depEqual(obj1, obj2) {
+
+//   // if (Object.keys(obj1).length != Object.keys(obj2).length) {
+//   //   return false;
+//   // };
+
+//   for (var propName in obj1) {
+//     if (obj1.hasOwnProperty(propName) && obj2.hasOwnProperty(propName)) {
+//       if (obj1[propName] === obj2[propName]) {
+//         return
+//       }
+//     }
+//   };
+
+// }
+
+// var objA = {
+//   string: 'Vasya',
+//   number: 30
+// };
+
+// var objB = {
+//   // string: 'Vasya',
+//   boolean: true,
+//   undefined: undefined,
+//   null: null
+// };
+
+// objA.__proto__ = objB;
+
+
+
+// console.log(depEqual(objA, objB));
