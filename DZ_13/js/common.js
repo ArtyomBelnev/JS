be3e17be036d9712f845b6002bac2e6ed228a279
Артем <@dom3.gsr.by>
2020-04-08 16:47:00 +0300

    привязка

---
+++ DZ_13/js/common.js
@@ -0,0 +1,82 @@
+var button = document.getElementsByClassName('button')[0];
+var user_tabs = document.getElementsByClassName('user_tabs')[0];
+var info_block = document.getElementsByClassName('info_block')[0];
+
+button.addEventListener('click', function () {
+	var xhr = new XMLHttpRequest();
+
+	xhr.open('GET', 'https://reqres.in/api/users?page=2', true);
+	xhr.send();
+
+	xhr.onload = function () {
+		var statusType = +String(this.status)[0];
+
+		if (statusType === 2) {
+			var dataUsers = JSON.parse(this.response).data;
+			userList(dataUsers);
+		}
+	};
+
+
+	xhr.onerror = function () {
+		console.error(this.status);
+	};
+
+
+	xhr.onloadend = function () {
+		console.log('Запрос завершен');
+	};
+});
+
+function userList(arr) {
+
+	// arr.forEach(function (item, i) {
+	// 	localStorage.setItem(item.id, JSON.stringify(item));
+	// });
+	for (var key in arr) {
+		localStorage.setItem(key, JSON.stringify(arr[key]));
+	}
+
+};
+
+for (var i = 1; i < localStorage.length + 1; i++) {
+	var newN = document.createElement('DIV');
+	newN.innerHTML = 'User ' + i;
+	user_tabs.appendChild(newN);
+	newN.id = i;
+	// var newDiv = document.createElement('DIV');
+	// newDiv.innerHTML = 'User ' + i;
+	// info_block.appendChild(newDiv);
+	// newDiv.classList.add('block');
+	// user_tabs.addEventListener('click', function addInput(event) {
+	// 	var target = event.target;
+
+	// 	if (target.tagName === 'DIV') {
+	// 		newB.classList.remove('block');
+	// 	}
+
+
+	// });
+}
+for (var j = 0; j < localStorage.length; j++) {
+	var photo = JSON.parse(localStorage.getItem(j)).avatar;
+
+	var newB = document.createElement('DIV');
+	info_block.appendChild(newB);
+	newB.classList.add('block');
+
+	var userPhoto = document.createElement('img');
+	userPhoto.setAttribute('src', photo);
+	newB.appendChild(userPhoto);
+}
+
+
+
+
+
+
+
+// var newDiv = document.createElement('DIV');
+// newDiv.innerHTML = 'User ' + i;
+// info_block.appendChild(newDiv);
+// newDiv.classList.toggle('block'); 
