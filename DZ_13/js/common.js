57b0819d60a14d4479d79a1131b9b983f7dd84b3
Belnev <belnevtema@gmail.com>
2020-04-09 22:51:40 +0300

    Ajax с LocalStorage

--- DZ_13/js/common.js
+++ DZ_13/js/common.js
@@ -2,26 +2,31 @@ var button = document.getElementsByClassName('button')[0];
 var user_tabs = document.getElementsByClassName('user_tabs')[0];
 var info_block = document.getElementsByClassName('info_block')[0];
 
+localStorage.clear();
+
 button.addEventListener('click', function () {
 	var xhr = new XMLHttpRequest();
 
+	// xhr.open('GET', 'https://reqres.in/api/users/23', true);
 	xhr.open('GET', 'https://reqres.in/api/users?page=2', true);
 	xhr.send();
 
 	xhr.onload = function () {
 		var statusType = +String(this.status)[0];
-
+		
 		if (statusType === 2) {
-			var dataUsers = JSON.parse(this.response).data;
-			userList(dataUsers);
+			if(localStorage.length === 0) {
+				var dataUsers = JSON.parse(this.response).data;
+				userList(dataUsers);
+			} 
+		} else if (statusType === 4) {
+			window.location.pathname = "/404.html";
 		}
 	};
-
 
 	xhr.onerror = function () {
 		console.error(this.status);
 	};
-
 
 	xhr.onloadend = function () {
 		console.log('Запрос завершен');
@@ -30,9 +35,6 @@ button.addEventListener('click', function () {
 
 function userList(arr) {
 
-	// arr.forEach(function (item, i) {
-	// 	localStorage.setItem(item.id, JSON.stringify(item));
-	// });
 	for (var key in arr) {
 		localStorage.setItem(key, JSON.stringify(arr[key]));
 	}
@@ -44,55 +46,65 @@ function userList(arr) {
 		newN.id = i;
 	}
 
-	
+	var id1 = user_tabs.firstElementChild.id;
+	addInfo(event, id1);
+	button.remove();
 };
 
-user_tabs.addEventListener('click', function addInput(event) {
-		var target = event.target.id;
-		var info = target - 1;
 
-		if (info_block.getElementsByTagName('DIV')[0]) {
-			info_block.getElementsByTagName('DIV')[0].remove();
-		}
-			var photo = JSON.parse(localStorage.getItem(info)).avatar;
+user_tabs.addEventListener('click', addInfo);
 
-			var newB = document.createElement('DIV');
-			info_block.appendChild(newB);
-			newB.classList.remove('block');
-	
-			var userPhoto = document.createElement('img');
-			userPhoto.setAttribute('src', photo);
-			newB.appendChild(userPhoto);
+function addInfo(event, id) {
+	var targe = event.target.id;
+	var info = targe - 1;
 
-	});
+	if (targe === undefined) {
+		info = id - 1;
+		user_tabs.firstElementChild.classList.add('red');
+	}
+
+	if (info_block.getElementsByTagName('DIV')[0]) {
+		info_block.getElementsByTagName('DIV')[0].remove();
+	}
+
+	var photo = JSON.parse(localStorage.getItem(info)).avatar;
+	var firstName = JSON.parse(localStorage.getItem(info)).first_name;
+	var lastName = JSON.parse(localStorage.getItem(info)).last_name;
+
+	var div1 = document.createElement('DIV');
+	info_block.appendChild(div1);
+	div1.classList.add('tabs')
+	div1.classList.remove('block');
+
+	var div2 = document.createElement('DIV');
+	div1.appendChild(div2);
+
+	var div3 = document.createElement('DIV');
+	div1.appendChild(div3);
+
+	var userPhoto = document.createElement('img');
+	userPhoto.setAttribute('src', photo);
+	div2.appendChild(userPhoto);
+
+	var inf = document.createElement('P');
+	inf.textContent = 'User  Name: ' + firstName;
+	div3.appendChild(inf);
+
+	var inf = document.createElement('P');
+	inf.textContent = 'User First Name: ' + lastName;
+	div3.appendChild(inf);
+}
+
+user_tabs.onclick = function (event) {
+	var target = event.target;
+
+	if (user_tabs.getElementsByClassName('red')[0]) {
+		user_tabs.getElementsByClassName('red')[0].classList.remove('red');
+	}
+	target.classList.add('red');
+};
 
 
 
-// for (var j = 0; j < localStorage.length; j++) {
-	
-// }
-	// var newDiv = document.createElement('DIV');
-	// newDiv.innerHTML = 'User ' + i;
-	// info_block.appendChild(newDiv);
-	// newDiv.classList.add('block');
-	// user_tabs.addEventListener('click', function addInput(event) {
-	// 	var target = event.target;
-
-	// 	if (target.tagName === 'DIV') {
-	// 		newB.classList.remove('block');
-	// 	}
-	
 
 
-
-
-
-
-
-
-
-
-// var newDiv = document.createElement('DIV');
-// newDiv.innerHTML = 'User ' + i;
-// info_block.appendChild(newDiv);
-// newDiv.classList.toggle('block'); 
